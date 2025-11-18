5a17721e5f9fda7d460aa6a03b5c369507273043
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2025-11-19 05:27:15 +0700

    styled the Header navlinks

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -1,23 +1,50 @@
-import { NavLink } from 'react-router-dom';
+import type React from 'react';
+import { NavLink, type NavLinkProps } from 'react-router-dom';
 import { faMoon, faSun } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+type NavButtonProps = {
+  to: NavLinkProps["to"]
+  end?: boolean
+  children: React.ReactNode
+}
+
+function NavButton({ to, end = false, children }: NavButtonProps) {
+  return (
+    <NavLink to={to} end={end}
+      className={({ isActive }) =>
+        isActive
+          ? "px-5 py-2 rounded-lg text-slate-200 bg-cyan-600 shadow-md transition"
+          : "px-5 py-2 rounded-lg text-slate-600 hover:text-slate-950 transition"
+      }
+    >
+      {children}
+    </NavLink>
+  )
+}
+
 function Header() {
   return (
-    <header className="bg-slate-100">
-      <section className="flex items-center justify-between px-6 py-4 w-full">
+    <header className="font-medium">
+      <section className="flex items-center justify-between px-6 py-7 w-full">
         {/* left header */}
         <div className="">
-          WYDO!
+          <span className="text-xl bg-cyan-600">WYDO</span>
         </div>
 
         {/* centre: navlinks */}
-        <div className="">
+        <div>
           <nav>
-            <ul className="flex gap-4">
-              <li><NavLink to="/" end>Home</NavLink></li>
-              <li><NavLink to="/predict">Predict!</NavLink></li>
-              <li><NavLink to="/about">About</NavLink></li>
+            <ul className="flex gap-6">
+              <li>
+                <NavButton to="/" end>Home</NavButton>
+              </li>
+              <li>
+                <NavButton to="/predict">Predict!</NavButton>
+              </li>
+              <li>
+                <NavButton to="/about">About</NavButton>
+              </li>
             </ul>
           </nav>
         </div>

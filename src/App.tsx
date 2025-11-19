dee77628fc82c85f434ec627a45794db71485d86
bbeetlesam <185041685+bbeetlesam@users.noreply.github.com>
2025-11-20 04:09:34 +0700

    add: lang context wrapper in App
    this so lang context and its state synced across all files and pages

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,22 @@ import { Route, Routes } from 'react-router-dom';
 import Footer from './components/layout/Footer';
 import Header from './components/layout/Header'
 import Home from './components/layout/Home'
+import { LangProvider } from './context/LangContext';
 
 function App() {
   return (
-    <div className="max-w-[100rem] mx-auto px-16">
-      <Header />
-      <div className="px-4">
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/predict" element={<Home />} />
-          <Route path="/about" element={<Home />} />
-        </Routes>
-      </div>
-      <Footer />
+    <div className="max-w-[110rem] mx-auto px-16 min-h-screen flex flex-col overflow-hidden">
+      <LangProvider>
+        <Header />
+        <div className="px-4">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/predict" element={<Home />} />
+            <Route path="/about" element={<Home />} />
+          </Routes>
+        </div>
+        <Footer />
+      </LangProvider>
     </div>
   )
 }
